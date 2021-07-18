ed0752b053002f865585e18430ba5697c969f023
unknown <mayercandelo.1985@gmail.com>
2021-07-17 19:03:30 -0500

    traemos el js y filtramos por type y size

---
+++ src/Readme.md
@@ -0,0 +1,33 @@
+---
+title: '<Pokémania>'
+disqus: hackmd
+---
+
+![](https://fontmeme.com/permalink/210715/10ca2a393eab02469d67854a3f33fbd9.png)
+
+
+## **Table of Contents**
+
+[TOC]
+
+
+## **Data Lover´S - POKEMANIA**
+
+### **1. Definición del producto**
+
+#### - **¿Quiénes son los principales usuarios de producto?**
+
+    Los fanaticos de Pokemon en un rango de edad de los 8 a los 30 años.
+    
+#### -   **¿Cuáles son los objetivos de estos usuarios en relación con el producto?**
+
+    Poder ver las caracteristicas de sus pokemones favoritos, filtrar los por sus habilidades, regiones, tipos, evolución, debilidades de combate, caracteristicas fisicas, etc.
+    
+#### -  **¿Cuáles son los datos más relevantes que quieren ver en la interfaz y por qué?**
+
+    Poder ver las tarjetas de sus pokemon con sus habilidades y caracteristicas y asi poder clasificarlos.
+#### -  **¿Cuándo utilizan o utilizarían el producto?**
+
+    Cuando quieran participar en batallas pokemon o 
+
+### **2. Historias de usuario**
--- src/main.js
+++ src/main.js
@@ -1,6 +1,64 @@
-import { example } from './data.js';
+import pokemon from './data/pokemon/pokemon.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+const listaDePokemon=pokemon;
 
-console.log(example, data);
+let mostrarPrimeros = function(n){
+    let pokemones=new Array();
+    for(let i=0; i < n; i++){
+        pokemones[i]=listaDePokemon.pokemon[i];
+        console.table(pokemones[i]);
+    }
+    return pokemones;
+
+} 
+/* mostrarPrimeros(2); */
+/* console.log(listaDePokemon) */
+/* Funcion pa filtrar los pokemon por size//mostraremos lo height <1.0 */
+let pokemonSize= function(){
+    let poke=new Object();
+    let pokemones=new Array();
+    for(let i=0;i<listaDePokemon.pokemon.length;i++){
+        poke=listaDePokemon.pokemon[i];
+        /*console.log(`numero: ${poke.num} nombre: ${poke.name} altura: ${poke.size.height}`);-*/
+        /*console.log(poke);*/
+        /*console.table(poke);*/
+        /*console.log(poke.size.height);*/
+        let alt=poke.size.height.replace(/[a-z]|[\s]/mg,"");
+        /*console.log(typeof(alt));
+        console.log(alt);*/
+        if(Number(alt)<=1){
+
+            /*console.log("hola entre al if");*/
+            pokemones.push(poke);
+            
+        }
+    } 
+    return pokemones;
+}
+
+/* console.table(pokemonSize()) */
+
+let pokemonForType=()=>{
+    /*let poke=new Object();*/
+    let pokemones=new Array();
+    listaDePokemon.pokemon.forEach(Element=>{
+        for(let i=0;i<Element.type.length;i++){
+            if(Element.type[i]=="grass"){
+                pokemones.push(Element);
+                console.log(Element);
+            }
+        }        
+    })
+    return pokemones;
+    
+}
+
+console.table(pokemonForType());
+
+
+
+
+
+/*console.table(mostrarPrimeros(2));*/
