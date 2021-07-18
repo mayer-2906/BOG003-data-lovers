5a0d0a84fb0b094fdfc3e706134d26321fe060cd
unknown <mayercandelo.1985@gmail.com>
2021-07-18 04:46:21 -0500

    cargamos los pokemones y generamos tarjeta

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import pokemon from './data/pokemon/pokemon.js';
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 const listaDePokemon=pokemon;
+let cantidadDePokemonesMostrados=0;
+
+/* console.log(typeof(listaDePokemon)); 
 
 let mostrarPrimeros = function(n){
     let pokemones=new Array();
@@ -12,17 +15,17 @@ let mostrarPrimeros = function(n){
     }
     return pokemones;
 
-} 
+} */
 /* mostrarPrimeros(2); */
 /* console.log(listaDePokemon) */
-/* Funcion pa filtrar los pokemon por size//mostraremos lo height <1.0 */
+/* Funcion para filtrar los pokemon por size//mostraremos lo height <1.0 */
 let pokemonSize= function(){
     let poke=new Object();
     let pokemones=new Array();
     for(let i=0;i<listaDePokemon.pokemon.length;i++){
         poke=listaDePokemon.pokemon[i];
         /*console.log(`numero: ${poke.num} nombre: ${poke.name} altura: ${poke.size.height}`);-*/
-        /*console.log(poke);*/
+        console.log(poke);
         /*console.table(poke);*/
         /*console.log(poke.size.height);*/
         let alt=poke.size.height.replace(/[a-z]|[\s]/mg,"");
@@ -39,7 +42,7 @@ let pokemonSize= function(){
 }
 
 /* console.table(pokemonSize()) */
-
+/* Funcion para filtrar los pokemon por type, caso de ejemplo grass */
 let pokemonForType=()=>{
     /*let poke=new Object();*/
     let pokemones=new Array();
@@ -55,9 +58,71 @@ let pokemonForType=()=>{
     
 }
 
-console.table(pokemonForType());
+/* console.table(pokemonForType()); */
 
+window.addEventListener('Load',cargarPokemones());
 
+function cargarPokemones(){
+
+    for(let i=cantidadDePokemonesMostrados;i<cantidadDePokemonesMostrados+250;i+=3){
+        /*crea una fila de tarjetas */
+        let fila=document.createElement("div");
+        fila.classList.add("filaPokemon");
+        for(let j=0;j<3;j++){
+            fila.appendChild(crearTarjeta(listaDePokemon.pokemon[i+j]));
+            /*cantidadDePokemonesMostrados++;*/
+        }
+        document.querySelector("#contenedor_Pokemones").appendChild(fila);
+    }
+    /*cantidadDePokemonesMostrados+=30;*/
+}
+
+/* 
+function crearfila(){
+} */
+
+function crearTarjeta(pokeTarj){
+
+    /* let nuevoPokemonTarjeta=[pokeTarj.name,pokeTarj.img,pokeTarj.type,pokeTarj.resistant];*/
+    let divTarjeta=document.createElement("div");
+    divTarjeta.classList.add("tarjetaPokemon");
+    let imgPokemon=document.createElement("img");
+    imgPokemon.classList.add("img_Pokemon");
+    imgPokemon.src=pokeTarj.img;
+    let nombrePokemon=document.createElement("p");
+    nombrePokemon.classList.add("nombre_pokemon");
+    nombrePokemon.textContent=`${pokeTarj.name}`;
+    let parrafoTarjeta=document.createElement("p");
+    parrafoTarjeta.classList.add("tipo_pokemon");
+    parrafoTarjeta.textContent=`Tipo:  ${obtenerType(pokeTarj.type)}`;
+    let parrafoResist=document.createElement("p");
+    parrafoResist.classList.add("resistencia_pokemon");
+    parrafoResist.textContent=` Resistencia:  ${obtenerResistencia(pokeTarj.resistant)}`
+    divTarjeta.appendChild(imgPokemon);
+    divTarjeta.insertAdjacentElement("afterbegin",nombrePokemon);
+    divTarjeta.insertAdjacentElement("beforeend",parrafoTarjeta);
+    divTarjeta.insertAdjacentElement("beforeend",parrafoResist);
+
+    return divTarjeta;
+    /* console.table(nuevoPokemonTarjeta); */
+}
+function obtenerType(tipo){
+    let tipos="";
+    for(let i=0;i<tipo.length;i++){
+        tipos=`${tipo[i]}  ${tipos} `;
+    }
+    return tipos;
+}
+
+function obtenerResistencia(resist){
+    let resistencias="";
+    for(let i=0;i<resist.length;i++){
+        resistencias=`${resist[i]}  ${resistencias} `;
+    }
+    return resistencias;
+}
+
+console.log(crearTarjeta(listaDePokemon.pokemon[3]));
 
 
 
